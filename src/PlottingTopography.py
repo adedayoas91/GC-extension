d86e293e0f1a8d6e48b8e6a10c32e42ba4d471ba
Sadiq Adedayo <adedayoas91@gmail.com>
2023-06-13 17:50:29 +0200

    Refactor into various classes

--- src/PlottingTopography.py
+++ src/PlottingTopography.py
@@ -1,64 +1,99 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_projections(corr,centers):
-    """
-    Plots the correlation of each ROIs with the behavior trace on the topography
-    Args:
-        corr: (array-like, matrix [n x n]): Correlation matrix of data
-        centers: (array-like, matrix []) Cell centers in
 
-    Returns:
+class Visualize_on_topography():
+    def __init__(self, connectivity_matrix, cell_centers,n_pasts) -> None:
+        self.conn_mat = connectivity_matrix
+        self.cell_centers = cell_centers
+        self.number_of_pasts = n_pasts
 
-    """
-    fig = plt.figure(figsize=(10, 6))
-    if centers.shape[1] == 2:
-        plt.scatter(centers[:, 0], centers[:, 1], marker = 'o', s = 10, c = corr, cmap = 'seismic')
-        plt.colorbar()
-    else:
-        ax = fig.add_subplot(111, projection = "3d")
-        p = ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], marker = 'o', s = 10, c = corr, cmap = 'seismic')
-        fig.colorbar(p)
+    def extract_coordinates(self):
+            """
+            Extracts 3D coordinates of each ROIs
+            :param inferred: shape [n x n] inferred matrix
+            :param topography: the position of ROIs given from data
+            :return: coordinates for each ROIs and the edges [t]
+            """
+            inferred, topography = self.conn_mat, self.cell_centers
+            t = np.transpose(np.where(inferred>0))
+            point_1 = np.zeros_like(t)
+            point_2 = np.zeros_like(t)
 
+            for i in range(len(t)):
+                point_1[i]=topography[t[i,0],[0,1]]
+                point_2[i]=topography[t[i,1],[0,1]]
 
-def plott_(inferred,n_past):
-    """
-    Plots connectivity matrix inferred into different matrices of corresponding pasts
-    Args:
-        inferred: (array-like: matrix [# of variables * n_pasts X # of variables]) connectivity matrix inferred
-        n_past: (int) number of pasts used in analysis
+            x_val,y_val,z_val = [],[],[]
+            for i in range(len(point_1)):
+                x_val.append([point_1[i,0],point_2[i,0]])
+                y_val.append([point_1[i,1],point_2[i,1]])
+                z_val.append([topography[t[i,0],2],topography[t[i,1],2]])
 
-    Returns: Plotted connectivity matrices corresponding to number of pasts
-
-    """
-    nn, n_neur = n_past + 1, inferred.shape[1]
-    fig, axs = plt.subplots(1, nn, figsize = (3.5 * nn, 3.5))
-    b = 0
-    for a in range(nn):
-        jj  = inferred[a * n_neur : (a + 1) * n_neur, b * n_neur : (b + 1) * n_neur]
-        axs[a].imshow(jj)
-        axs[a].axis('off')
-    plt.tight_layout()
+            return x_val,y_val,z_val, t
 
 
 
-def plot_Inferred_3D(inferred,centers,arr):
-    """
+    def plot_correlation_of_ROIs_with_behavior(self, corr_list):
+        """
+        Plots the correlation of each ROIs with the behavior trace on the topography
+        Args:
+            corr_list: (array-like, matrix [n x n]): Correlation matrix of data
+            centers: (array-like, matrix []) Cell centers in
 
-    :param inferred:
-    :param centers:
-    :param arr:
-    :return:
-    """
-    x_val,y_val,z_val = xyz_maker(inferred,centers)
-    fig = plt.figure(figsize = (10,10))
-    ax = fig.add_subplot(111,projection="3d")
-    ax.scatter(centers[:,0],centers[:,1],centers[:,2],color='red',s=10,marker='.')
-    ax.scatter(centers[arr,0],centers[arr,1],centers[arr,2],color='green',s=15,marker='*')
-    for a in range(len(x_val)):
-        ax.plot(x_val[a],y_val[a],z_val[a],lw=0.7,alpha=.7)
-    ax.grid(False)
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-    ax.set_zlabel('z-axis')
-    return fig
+        Returns:
+
+        """
+        centers = self.cell_centers
+        fig = plt.figure(figsize=(10, 6))
+        if centers.shape[1] == 2:
+            plt.scatter(centers[:, 0], centers[:, 1], marker = 'o', s = 10, c = corr_list, cmap = 'seismic')
+            plt.colorbar()
+        else:
+            ax = fig.add_subplot(111, projection = "3d")
+            p = ax.scatter(centers[:, 0], centers[:, 1], centers[:, 2], marker = 'o', s = 10, c = corr_list, cmap = 'seismic')
+            fig.colorbar(p)
+
+
+    def plot_connectivity_matrix_at_all_lags(self):
+        """
+        Plots connectivity matrix inferred into different matrices of corresponding pasts
+        Args:
+            inferred: (array-like: matrix [# of variables * n_pasts X # of variables]) connectivity matrix inferred
+            n_past: (int) number of pasts used in analysis
+
+        Returns: Plotted connectivity matrices corresponding to number of pasts
+
+        """
+        inferred, n_past = self.conn_mat, self.number_of_pasts 
+        nn, n_neur = n_past + 1, inferred.shape[1]
+        fig, axs = plt.subplots(1, nn, figsize = (3.5 * nn, 3.5))
+        b = 0
+        for a in range(nn):
+            jj  = inferred[a * n_neur : (a + 1) * n_neur, b * n_neur : (b + 1) * n_neur]
+            axs[a].imshow(jj)
+            axs[a].axis('off')
+        plt.tight_layout()
+
+
+
+    def plot_connectivity_matrix_of_identified_ROIs_in_volume(inferred,centers,arr):
+        """
+
+        :param inferred:
+        :param centers:
+        :param arr:
+        :return:
+        """
+        x_val,y_val,z_val = self.extract_coordinates(inferred,centers)
+        fig = plt.figure(figsize = (10,10))
+        ax = fig.add_subplot(111,projection="3d")
+        ax.scatter(centers[:,0],centers[:,1],centers[:,2],color='red',s=10,marker='.')
+        ax.scatter(centers[arr,0],centers[arr,1],centers[arr,2],color='green',s=15,marker='*')
+        for a in range(len(x_val)):
+            ax.plot(x_val[a],y_val[a],z_val[a],lw=0.7,alpha=.7)
+        ax.grid(False)
+        ax.set_xlabel('X-axis')
+        ax.set_ylabel('Y-axis')
+        ax.set_zlabel('z-axis')
+        return fig
--- src/dataPreProcessing.py
+++ src/dataPreProcessing.py
@@ -1,20 +1,31 @@
 import numpy as np
 
-def replace_nan_(data, frames, delete_frames = bool):
-    """
-    Deletes NAN frames in data
-    Args:
-        data: (array-like: matrix [# of variables X # of samples])
-        frames: (array-like: vector) Index of identified defected frames
-        delete_frames:
+class PreProcess_data():
+    def __init__(self, arr, n_past, bad_frames=bool):
+        self.original_data = arr.copy()
+        self.orig_arr_sorted = self.sort_ROI_to_rows()
+        self.arr = None
 
-    Returns: Data without bad frames
+    def sort_ROI_to_rows(self):
+        i, j = self.original_data.shape()
+        if i > j:
+            self.arr = self.original_data.T
+    
+    def replace_nan_(self, bad_frames, delete_frames = bool):
+        """
+        Deletes NAN frames in data and if delete_frames is False, it interpolates to fill the bad frames
+        Args:
+            data: (array-like: matrix [# of variables X # of samples])
+            bad_frames: (array-like: vector) Index of identified defected frames
+            delete_frames: (bool) If true, deletes frames and if False, interpolates
 
-    """
-    if delete_frames:
-        data = np.delete(data,np.array(frames),axis=1)
-    else:
-        for a in range(data.shape[0]):
-            for i in frames:
-                data[a,i] = (data[a,i-1]+data[a,i+1])/2
-    return data
+        Returns: Data without bad frames
+        """
+        data = self.arr
+        if delete_frames:
+            data = np.delete(data, np.array(bad_frames), axis=1)
+        else:
+            for a in range(data.shape[0]):
+                for i in bad_frames:
+                    data[a,i] = (data[a,i-1]+data[a,i+1])/2
+        return data
