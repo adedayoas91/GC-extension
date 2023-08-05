619aad2204d77190eddb4f477d6d773eb54a76fd
Sadiq Adedayo <adedayoas91@gmail.com>
2023-08-05 12:29:32 +0200

    refactored preprocessing

--- src/dataPreProcessing.py
+++ src/dataPreProcessing.py
@@ -1,53 +1,47 @@
 import numpy as np
 
-class PreProcess_data():
-    def __init__(self, arr:np.ndarray, n_past:int=0, bad_frames:np.ndarray=None):
-        self.orig_arr=arr.copy()
-        self.n_past=n_past
-        self.arr = self.sort_ROI_to_rows()
-        self.bad_frames = bad_frames
 
-    def sort_ROI_to_rows(self):
-        i, j = self.orig_arr.shape()
-        if i > j:
-            self.arr = self.orig_arr.T
-    
-    def replace_nan_(self, bad_frames:int, delete_frames:bool):
-        """
-        Deletes NAN frames in data and if delete_frames is False, it interpolates to fill the bad frames
-        Args:
-            data: (array-like: matrix [# of variables X # of samples])
-            bad_frames: (array-like: vector) Index of identified defected frames
-            delete_frames: (bool) If true, deletes frames and if False, interpolates
+def replace_nan(arr: np.ndarray, bad_frames: np.ndarray, delete_frames: bool) -> np.ndarray:
+    """
+    Deletes NAN frames in data and if delete_frames is False, it interpolates to fill the bad frames
+    Args:
+        arr: # TODO: specify that the array needs to be in the right orientation
+        bad_frames:
+        delete_frames: (bool) If true, deletes frames and if False, interpolates
 
-        Returns: Data without bad frames
-        """
-        if delete_frames:
-            self.arr = np.delete(self.arr, np.array(bad_frames), axis=1)
-        else:
-            for a in range(self.arr.shape[0]):
-                for i in bad_frames:
-                    self.arr[a,i] = (self.arr[a,i-1]+self.arr[a,i+1])/2
-        return
+    Returns: Data without bad frames
+    """
+    if delete_frames:
+        return np.delete(arr, bad_frames, axis=1)
 
-    
-    def make_shifted_versions_of_data(self):
-        """
-        Creates shifted versions of original data based on the number of pasts nn required.
-        Concatenate the shifted arrays and return the new data.
-        Args:
-            X: (array-like, shape [# of variable X # of samples]): Raw data
-            nn: (int) number of shifted version of data required
+    arr_copy = arr.copy()
+    for var in range(arr_copy.shape[0]):
+        for i in bad_frames:
+            arr_copy[var, i] = (arr_copy[var, i - 1] + arr_copy[var, i + 1]) / 2
 
-        Returns: shifted data
+    return arr_copy
 
-        """
-        
-        if self.n_past == 0:
-            return self.arr
-        else:
-            X_ = self.arr[:, (self.n_past):]
-            for i in range(self.n_past):
-                idx1, idx2 = self.n_past-1-i, -i-1
-                X_ = np.r_[X_, self.arr[:, idx1:idx2]]
-            return X_
+
+def shift_data(arr: np.ndarray, n_past: int) -> np.ndarray:
+    """
+    Creates shifted versions of original data based on the number of pasts nn required.
+    Concatenate the shifted arrays and return the new data.
+    Args:
+        arr:
+        n_past:
+
+    Returns:
+
+    """
+
+    if n_past == 0:
+        return arr
+
+    trimmed_arr = arr[:, n_past:]
+    for i in range(n_past):
+        idx1 = n_past - 1 - i
+        idx2 = -i - 1
+
+        trimmed_arr = np.r_[trimmed_arr, arr[:, idx1:idx2]]
+
+    return trimmed_arr
